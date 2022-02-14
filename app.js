e495f07cc12e323b00af9ba057c0c7025af6efed
Mary Martinez <mary.diana.martinez@gmail.com>
2022-02-14 11:16:32 -0800

    signUpForm event listener added and functioning

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { signupUser } from './fetch-utils.js';
 const signInForm = document.getElementById('sign-in');
 const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
@@ -7,5 +8,12 @@ const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
 
 // Wire up sign in and sign up forms to supabase
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    // console.log(signUpEmail.value, signUpPassword.value);
+    const resp = await signupUser(signUpEmail.value, signUpPassword.value);
+    console.log(resp);
+});
+
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://nxkfcrgevcuksvdnaugc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54a2ZjcmdldmN1a3N2ZG5hdWdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0NjAsImV4cCI6MTk1OTkxNzQ2MH0.9e3rnFUxMChSkwzJQcBImTDSwY_4VDtfmLDUZ-dY7gc';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,7 +8,9 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    
+}
 
 export async function signInUser(email, password) {}
 
